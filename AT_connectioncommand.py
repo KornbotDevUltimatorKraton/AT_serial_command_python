4945b262908106c3c55f7a7c68566e36d4d910a3
Chanapai Chuadchum <kornbot359@hotmail.com>
2022-05-28 13:27:23 +0700

    Add files via upload

---
+++ AT_connectioncommand.py
@@ -0,0 +1,15 @@
+import serial 
+ser = serial.Serial("/dev/ttyUSB0")
+ser.write('AT +SAPBR = 3,1,"CONTYPE","GPRS"\n;'.encode('UTF-8'))
+ser.write('AT +SAPBR = 3,1,"APN","RCMNET"\n;'.encode('UTF-8'))
+ser.write('AT +SAPBR = 1,1\n;'.encode('UTF-8'))
+ser.write('AT +SAPBR= 1,1\n;'.encode('UTF-8'))
+ser.write('AT +SAPBR= 2,1\n;'.encode('UTF-8'))
+ser.write('AT +CIPGSMLOC=1,1\n;'.encode('UTF-8'))
+ser.write('AT +CIPGSMLOC=1,1\n;'.encode('UTF-8'))
+ser.write('AT +CIPGSMLOC=1,1\n;'.encode('UTF-8'))
+ser.write('AT +CIPGSMLOC=1,1\n;'.encode('UTF-8'))
+ser.write('AT +CIPGSMLOC=1,1\n;'.encode('UTF-8'))
+ser.write('AT +CIPGSMLOC=1,1\n;'.encode('UTF-8'))
+ser.write('AT +CIPGSMLOC=1,1\n;'.encode('UTF-8'))
+
