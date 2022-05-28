e712913fdafecf9f687cbaf4288e4f02c45c88ba
Chanapai Chuadchum <kornbot359@hotmail.com>
2022-05-28 14:38:01 +0700

    Add files via upload

--- AT_connectioncommand.py
+++ AT_connectioncommand.py
@@ -11,5 +11,15 @@ ser.write('AT +CIPGSMLOC=1,1\n;'.encode('UTF-8'))
 ser.write('AT +CIPGSMLOC=1,1\n;'.encode('UTF-8'))
 ser.write('AT +CIPGSMLOC=1,1\n;'.encode('UTF-8'))
 ser.write('AT +CIPGSMLOC=1,1\n;'.encode('UTF-8'))
-ser.write('AT +CIPGSMLOC=1,1\n;'.encode('UTF-8')) # Command to connect and get the location function 
+ser.write('AT +CIPGSMLOC=1,1\n;'.encode('UTF-8'))
 
+if ser.readline().decode('UTF-8').split(" ")[0] == "+CIPGSMLOC:":
+              loc = ser.readline().decode('UTF-8').split(" ")[1]
+              print("Long,Lat:",loc) # getting the location from the tuple of data 
+
+
+#Diable sudo usage command 
+export DPT_ALLOW_FAKEROOT_OR_SUDO=false
+export DPT_CHOWN_FILES=false
+export DPT_RESET_SETGID=false
+export DPT_SUDO=false
